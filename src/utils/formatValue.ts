a052b20f7e8ca02492ae7009068a33dd6973c9aa
Teuuz1994 <matheus.rj.ig@gmail.com>
2020-07-29 20:24:12 -0300

    feat: challenge complete :rocket:

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,15 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  if (value === 0 || value === undefined) {
+    return Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(0);
+  }
+
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
 
 export default formatValue;
